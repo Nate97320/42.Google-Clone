75130d0a126a72bccd039a03f8bbaafc647ab33d
NEYTULLAH BAHADIR <nate-nate86@hotmail.com>
2022-04-21 23:47:55 +0300

    google cloned

---
+++ craco.config.js
@@ -0,0 +1,15 @@
+// craco.config.js
+
+module.exports = {
+  style: {
+    postOptions: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+      eslint: {
+          enable: false
+      },
+    },
+  },
+};
